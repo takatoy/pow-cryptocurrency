21090d2f6764dda38e7f1c28b4a1f9a34a34e632
The Three Kings <keitasanno@users.noreply.github.com>
2017-06-02 10:35:17 +0900

    Add a request handler

---
+++ src/WaffleRequestHandler.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+class WaffleRequestHandler extends HttpServlet {
+	public void getRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		response.setContentType("text/html;charset=Shift_JIS");
+
+		String command = request.getParameter("command");
+		String parameter = request.getParameter("parameter");
+
+		switch (command) {
+			case command:
+				statements;
+				break;
+			default:
+				break;
+		}
+		
+		PrintWriter pw = response.getWriter();
+		StringBuffer sb = new StringBuffer();
+		
+		pw.println(new String(sb));
+		pw.close();
+	}
+}
